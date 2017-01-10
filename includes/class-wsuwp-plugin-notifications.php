1a4bbb25bd88b63ed17ad19bb03a4c31fbe32af4
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-10 12:59:08 -0800

    Provide a method for sending a Slack notification
    This allows code elsewhere to send a Slack notification by passing
    a room, text, and a context.
    
    In its current form, `$data` is a string. This will be expanded in
    the future to support an array for more complex notifications.

--- includes/class-wsuwp-plugin-notifications.php
+++ includes/class-wsuwp-plugin-notifications.php
@@ -28,4 +28,41 @@ class WSUWP_Plugin_Notifications {
 	 * @since 0.0.1
 	 */
 	public function setup_hooks() {}
+
+	/**
+	 * Sends a notification to a Slack Webhook.
+	 *
+	 * @since 0.0.1
+	 *
+	 * @param string $room    The name (without hashtag) of the room.
+	 * @param string $data    The text of the notification. In a future version, this will support an array.
+	 * @param string $context The context of the notification.
+	 *
+	 * @return bool True if successful, False if not.
+	 */
+	public function send_slack_notification( $room, $data, $context = 'default' ) {
+		$message = array(
+			'channel' => '#' . sanitize_key( $room ),
+			'mrkdown' => true,
+			'username' => 'wsuwp-notifications',
+			'text' => $data,
+			'icon_emoji' => ':saxophone:',
+		);
+
+		$payload = 'payload=' . wp_json_encode( $message );
+
+		$url = apply_filters( 'wsuwp_slack_notification_url', '', $context, $room, $data );
+
+		if ( empty( $url ) ) {
+			return false;
+		}
+
+		$response = wp_remote_post( $url, $payload );
+
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		return true;
+	}
 }
